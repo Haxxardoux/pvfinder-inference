561b2a4718baf68ea25ae6c6e50674d6cf65a436
Will Tepe(Mike student) <will@sleepy.geop.uc.edu>
2021-05-03 20:10:34 -0400

    Added dataloader for vanilla pv-finder models

---
+++ dataloader.py
@@ -0,0 +1,86 @@
+import jax
+import torch
+import warnings
+
+
+# This can throw a warning about float - let's hide it for now.
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=FutureWarning)
+    import h5py
+
+
+
+class KDE_to_PV_Dataset(torch.utils.data.Dataset):
+    """
+    This class collects data. It does not split it up. You can pass in multiple files.
+    Example: collect_data('a.h5', 'b.h5')
+
+    batch_size: The number of events per batch
+    dtype: Select a different dtype (like float16)
+    slice: Allow just a slice of data to be loaded
+    device: The device to load onto (CPU by default)
+    masking: Turn on or off (default) the masking of hits.
+    **kargs: Any other keyword arguments will be passed on to torch's DataLoader
+    """
+
+    def __init__(
+        self,
+        *files,
+        dtype=jax.numpy.float32,
+        masking=False,
+        slice=None,
+        load_xy=False,
+        **kargs,
+    ):
+
+    
+        Xlist = []
+        Ylist = []
+
+        print("Loading data...")
+        for XY_file in files:
+            with h5py.File(XY_file, mode="r") as XY:
+                ## .expand_dims(axis=1) makes X (a x b) --> (a x 1 x b) (axis 0, axis 1, axis 2)
+                X = jax.numpy.expand_dims(jax.numpy.asarray(XY["kernel"]), axis=1).astype(dtype)
+                Y = jax.numpy.asarray(XY["pv"]).astype(dtype)
+
+
+                if load_xy:
+                    x = jax.numpy.expand_dims(jax.numpy.asarray(XY["Xmax"]), axis=1).astype(dtype)
+                    y = jax.numpy.expand_dims(jax.numpy.asarray(XY["Ymax"]), axis=1).astype(dtype)
+                    x = x*(X != 0)
+                    y = y*(X != 0)
+                    X = jax.numpy.concatenate((X, x, y), axis=1)  ## filling in axis with (X,x,y)
+
+                if masking:
+                    # Set the result to nan if the "other" array is above
+                    # threshold and the current array is below threshold
+                    Y = jax.ops.index_update(Y, ((jax.numpy.asarray(XY["pv_other"]) > 0.01) & (Y < 0.01)), jax.numpy.nan)
+#                     Y = Y * jax.numpy.nan * ((jax.numpy.asarray(XY["pv_other"]) > 0.01) & (Y < 0.01))
+
+                Xlist.append(X)
+                Ylist.append(Y)
+
+        X = jax.numpy.concatenate(Xlist, axis=0)
+        Y = jax.numpy.concatenate(Ylist, axis=0)
+
+        if slice:
+            X = X[slice, :, :]
+            Y = Y[slice, :]
+        
+        self.X = X
+        self.Y = Y
+        
+        print('Loaded data!')
+
+    def __getitem__(self, i):
+        return self.X[i], self.Y[i]
+
+    def __len__(self):
+        return len(self.X)
+
+if __name__ == '__main__':
+    train_dataset = KDE_to_PV_Dataset('/share/lazy/will/data/June30_2020_80k_1.h5', masking=True, load_xy=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = 16)
+    
+    print("Passed test")
